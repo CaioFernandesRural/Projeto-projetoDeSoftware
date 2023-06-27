69f8e9d7bcdb2af5ebf285169074eeb9ee6b18d0
Lorena <lorenavasconcells@gmail.com>
2023-06-27 10:29:48 -0300

    frontend

--- laravel-angular/resources/frontend/angular/src/app/app.module.ts
+++ laravel-angular/resources/frontend/angular/src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
+import { CadastroLivroAnuncioComponent } from './cadastro-livro-anuncio/cadastro-livro-anuncio.component';
+import { HistoricoEmprestimosComponent } from './historico-emprestimos/historico-emprestimos.component';
+import { LivroAnuncioComponent } from './livro-anuncio/livro-anuncio.component';
+import { LivrosComponent } from './livros/livros.component';
+import { PerfilUserComponent } from './perfil-user/perfil-user.component';
+import { RenovarEmprestimoComponent } from './renovar-emprestimo/renovar-emprestimo.component';
+import { SolicitarEmprestimoComponent } from './solicitar-emprestimo/solicitar-emprestimo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginUserComponent,
+    CadastroUserComponent,
+    CadastroLivroAnuncioComponent,
+    HistoricoEmprestimosComponent,
+    LivroAnuncioComponent,
+    LivrosComponent,
+    PerfilUserComponent,
+    RenovarEmprestimoComponent,
+    SolicitarEmprestimoComponent
   ],
   imports: [
     BrowserModule
---
+++ laravel-angular/resources/frontend/angular/src/app/renovar-emprestimo/renovar-emprestimo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renovar-emprestimo',
+  templateUrl: './renovar-emprestimo.component.html',
+  styleUrls: ['./renovar-emprestimo.component.css']
+})
+export class RenovarEmprestimoComponent {
+
+}
