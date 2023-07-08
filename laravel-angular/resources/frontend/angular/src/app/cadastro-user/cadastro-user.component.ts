efd2f5e39635d14d8436ecfe932688724de3cc83
Lorena <lorenavasconcells@gmail.com>
2023-07-07 22:15:08 -0300

    cadastro formGroup

--- laravel-angular/resources/frontend/angular/src/app/cadastro-user/cadastro-user.component.ts
+++ laravel-angular/resources/frontend/angular/src/app/cadastro-user/cadastro-user.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-user',
   templateUrl: './cadastro-user.component.html',
   styleUrls: ['./cadastro-user.component.css']
 })
-export class CadastroUserComponent {
+export class CadastroUserComponent implements OnInit {
+
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder,) {
+    this.registerForm = this.initializeRegisterForm();
+  }
+
+  initializeRegisterForm() {
+    return this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required, Validators.minLength(6)],
+      telefone: ['', Validators.required],
+      idade: [0, Validators.required],
+      sexo: ['', Validators.required],
+      cidade: ['', Validators.required],
+      estado: ['', Validators.required],
+      bio: ['', Validators.required],
+      data_nasc: [null, Validators.required],
+      admin: [false, Validators.required],
+      passwordConfirm: ['', Validators.required]
+    }, {
+      validator: this.mustMatch('password', 'passwordConfirm')
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeRegisterForm();
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  submit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+  }
+
+  //Função para confirmar senha
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null)
+      }
+    };
+  }
 
 }
---
+++ laravel-angular/resources/frontend/angular/src/app/models/register.model.ts
@@ -0,0 +1,14 @@
+export class Register {
+    nome: string = '';
+    email: string = '';
+    password: string = '';
+    telefone: string = '';
+    idade: number = 0;
+    sexo: string = '';
+    cidade: string = '';
+    estado: string = '';
+    bio: string = '';
+    data_nasc: Date | null = null;
+    admin: boolean = false;
+  }
+  
