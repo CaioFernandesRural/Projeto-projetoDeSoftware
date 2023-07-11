d50d994e109328f815f24f12fd85670b01214fc3
Caio Fernandes <caioorebeo@gmail.com>
2023-07-11 00:26:19 -0300

    fabricas tão quente

--- Api/database/seeders/DatabaseSeeder.php
+++ Api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Livro;
+use App\Models\Anuncio;
+use App\Models\Categoria;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //cria solto
+        Livro::factory(20)->create();
+        User::factory(20)->create();
+        Categoria::factory(30)->create([
+            'idLivro' => Livro::factory()->create()->id
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //pra criar anuncio sem requerente
+        User::factory(30)->create()->each(function($usuario){
+            $livro = Livro::factory()->create();
+            Anuncio::factory()->create([
+                'idDono' => $usuario->id,
+                'idLivro' => $livro->id
+            ]);
+        });
+
+        //pra criar anuncio com requerente
+        User::factory(30)->create()->each(function($usuario){
+            $livro = Livro::factory()->create();
+            $requerente = User::factory()->create();
+            Anuncio::factory()->create([
+                'idDono' => $usuario->id,
+                'idRequerente' => $requerente->id,
+                'idLivro' => $livro->id
+            ]);
+        });
     }
 }
